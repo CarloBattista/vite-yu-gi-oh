16d4b3077e15be00e4c0ac4ccba00d696a1befde
CarloBattista <carlitobatti@gmail.com>
2023-05-05 14:19:48 +0200

    stile header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <h1>header</h1>
+        <div class="header">
+            <a href="/">
+                <img class="logo" src="/_resources/yugioh_logo.svg" alt="logo yu-gi-oh">
+                <span class="heading">Yu-Gi-Oh Api</span>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -11,5 +16,28 @@
 </script>
 
 <style lang="scss" scoped>
-
+.header{
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    height: 70px;
+    padding: 0 60px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    background: #fff;
+    a{
+        display: flex;
+        align-items: center;
+        .logo{
+            width: 90px;
+            margin-right: 2em;
+        }
+        .heading{
+            color: #000;
+            font-size: 1.5rem;
+            font-weight: 700;
+        }
+    }
+}
 </style>
